4b653a2dd0f1f2a7753246a544186a673ac0c824
atefehabiarian <166217945+atefehabiarian@users.noreply.github.com>
2026-07-27 12:43:55 +0330

    a

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,21 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import { UserLogInComponent } from './user login panel/user-login.component';
+import { UserLoginComponent } from './user login panel/user-login.component';
 import { UserSignUpComponent } from './user sign up panel/user-signup.component';
 
 @NgModule({
-    declarations: [
-        UserLogInComponent,
-        UserSignUpComponent
-    ],
-    imports: [
-        CommonModule,
-        ButtonModule,
-
-    ],
-    exports: [
-        UserLogInComponent,
-        UserSignUpComponent],
+  declarations: [UserLoginComponent, UserSignUpComponent],
+  imports: [CommonModule, ButtonModule],
+  exports: [UserLoginComponent, UserSignUpComponent],
 })
-export class SidebarModule { }
+export class SidebarModule {}
